d76bf4c79161b873411494db4ae68d3422e6c098
gonzalodutto <gonzalodutto@gmail.com>
2021-05-31 10:40:52 -0300

    #145

--- script.js
+++ script.js
@@ -61,6 +61,23 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+const displayMovements = function (movements) {
+  containerMovements.innerHTML = '';
+  movements.forEach(function (mov, i) {
+    const type = mov > 0 ? 'deposit' : 'withdrawal';
+    const html = `
+        <div class="movements__row">
+          <div class="movements__type movements__type--${type}">${
+      i + 1
+    } ${type}</div>
+          <div class="movements__value">${mov}€</div>
+        </div>`;
+    containerMovements.insertAdjacentHTML('afterbegin', html);
+  });
+};
+
+displayMovements(account1.movements);
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
@@ -140,6 +157,7 @@ movements.forEach(function (mov, i, arr) {
 /////////////////////////////////////////////////
 // Looping arrays: forEach
 
+/*
 // Map
 const currencies = new Map([
   ['USD', 'United States dollar'],
@@ -157,3 +175,4 @@ console.log(currenciesUnique);
 currenciesUnique.forEach(function (value, _, map) {
   console.log(`${value}: ${value}`);
 });
+*/
